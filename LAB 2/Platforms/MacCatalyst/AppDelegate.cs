a5b97deba4099e615deaf55d8812a852f203e15b
ajdinbukvic <83658573+ajdinbukvic@users.noreply.github.com>
2023-01-16 05:27:39 +0100

    Add files via upload

---
+++ LAB 2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AjdinBukvic_RMA_LAB2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
