cd47f370739277faba42a9ce0844b5eb3d2037ee
ajdinbukvic <83658573+ajdinbukvic@users.noreply.github.com>
2023-01-16 05:33:09 +0100

    Add files via upload

---
+++ LAB 3/AjdinBukvicReallyFastFoodApp/Models/Jelo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjdinBukvicReallyFastFoodApp.Models
+{
+    public class Jelo
+    {
+        public int Id { get; set; }
+        public string Icon { get; set; }
+        public string Naziv { get; set; }
+        public int Cijena { get; set; }
+        public int Kalorije { get; set; }
+        public bool Odabrano { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
---
+++ LAB 3/AjdinBukvicReallyFastFoodApp/Models/Naselje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjdinBukvicReallyFastFoodApp.Models
+{
+    public class Naselje
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public int Udaljenost { get; set; }
+    }
+}
